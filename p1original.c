c09eaed9bfab1e05f43b3ac98b2b9cdf73007ed7
Thabasun24 <94883228+Thabasun24@users.noreply.github.com>
2022-02-08 04:27:53 +0000

    execute

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
-
+#include<stdio.h>
+void input(float*base,float*height)
+{
+  printf("enter the lenght of base:");
+  scanf("%f" ,base);
+  printf("enter the lenght of height:");
+  scanf("%f" ,height);
 }
+void find_area(float base,float height,float *area)
+{
+  *area=((base*height)/2);
+}
+void output(float base,float height,float area)
+{
+  printf("area of traingle is %f * %f/2 is %f", base,height,area);
+}
+int main()
+{
+  float x,y,z;
+  input(&x,&y);
+  find_area(x,y,&z);
+  output(x,y,z);
+    
+  return 0;
+  }
+
